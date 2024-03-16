39b81d9efc6fbff0558fd2e2f451bbb8afee1e4b
Tim S <t.suellner@web.de>
2024-03-16 02:15:44 +0100

    fix: removed special route for favicon

--- backend/src/controller/frontend/frontend.controller.ts
+++ backend/src/controller/frontend/frontend.controller.ts
@@ -6,13 +6,6 @@ import {join} from "path";
 @Controller()
 @ApiTags('frontend')
 export class FrontendController {
-    @Get('/favicon.ico')
-    @ApiDefaultResponse({
-        description: "This endpoint provides the icon for the frontend"
-    })
-    catchFavicon(@Res() response: Response) {
-        response.sendFile(join(__dirname, '..', '..', "..", "..", 'frontend', 'dist', 'assets', 'favicon.ico'));
-    }
     @Get()
     @ApiDefaultResponse({
         description: "This endpoint provides access to to the frontend"
